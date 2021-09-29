edd4c224171846171b1392268bea841d3b4811b5
k1574 <k1574@yandex.ru>
2021-09-30 00:21:52 +0500

    Init.

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void
+usage(void)
+{
+	fprintf(stderr, "usage: %s <x1> <y1> <x2> <y2>\n");
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int px, py, x1, y1, x2, y2, dx, dy;
+	float x, y, l, c, dl, dlb;
+
+	if(argc!=5)
+		usage();
+
+	x1 = atoi(argv[1]) ; 
+	y1 = atoi(argv[2]) ;
+	x2 = atoi(argv[3]) ;
+	y2 = atoi(argv[4]) ;
+	
+	dl = 1. ;
+	dlb = .05 ;
+	dx = x2 - x1 ;
+	dy = y2 - y1 ;
+	c = sqrt(dx*dx + dy+dy) ;
+
+	printf("%d %d\n", x1, y1);
+	px = x1 ; py = y1 ;
+	l = dl ;
+	do{
+		x = (float)x1 + (l/c) * dx ;
+		y = (float)y1 + (l/c) * dy ;
+
+		/* Every next point must differ max' for 1. */
+		while(abs(px - (int)x) > 1 || abs(py - (int)y) > 1){
+			l -= dlb ;
+			continue;
+		}
+
+		/* Skip repeating values. */
+		if(px != (int)x || py != (int)y )
+			printf("%d %d\n", (int)x, (int)y);
+
+		l += dl ;
+		px = (int)x ;
+		py = (int)y ;
+	}while(l <= c);
+
+	if((int)x != x2 || (int)y!=y2)
+		printf("%d %d\n", x2, y2);	
+
+	return 0 ;
+}
